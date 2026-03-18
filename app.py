b41f92741815cd4a86f495b3aa3e59f404eb6d7e
AceTheDeveloper <ghostwrexker@gmail.com>
2026-03-18 17:55:31 +0800

    Added the port again and again

--- app.py
+++ app.py
@@ -94,5 +94,5 @@ def check_order():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))

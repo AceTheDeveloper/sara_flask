a1cf024ae3e658995125c2275694820daf811374
AceTheDeveloper <ghostwrexker@gmail.com>
2026-03-18 11:25:15 +0800

    ello World

--- app.py
+++ app.py
@@ -5,7 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on Render!"
+    return {"message" : "hello"}
+
+@app.get('/get_menu')
+def get_menu():
+    return {
+        "menu" : "Menu"
+    }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
